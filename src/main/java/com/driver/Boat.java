f5158a1413ee9944ce40e04c0d4e20e7146c41bd
v-teja <tejaviswa95@gmail.com>
2022-12-17 16:15:12 +0530

    changes made

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,13 +1,23 @@
 package com.driver;
 
 public class Boat implements WaterVehicle{
+
     @Override
     public String getVehicleName() {
-        return "BOAT";
+        return VehicleName;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return 10;
+        return VehicleCapacity;
     }
+
+    private String VehicleName;
+    private int VehicleCapacity;
+
+    public Boat(String name, int capacity){
+            this.VehicleName = name;
+            this.VehicleCapacity = capacity;
+    }
+
 }
